042e9b190affafdc5feca90d57bf8999f2bf027c
Dhananjay Sharma <56606048+dhnanj2@users.noreply.github.com>
2021-02-22 14:12:31 +0530

    Update FileIO.cpp

--- FileIO.cpp
+++ FileIO.cpp
@@ -2,19 +2,60 @@
 
 using namespace std;
 
-template<typename T>
-vector<T> read_vector(ifstream &in) {
-    vector<T> ans;
-    T temp;
-    while(in>>temp) {
-        ans.push_back(temp);
-        in>>ws;
+#define pb push_back
+
+#define all(c) (c).begin(), (c).end()
+
+#define sz(x) (int)(x).size()
+
+#define FOR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+
+#define pii pair<int,int>
+
+#define ll long long 
+
+#define pb push_back
+
+#define S second
+
+#define F first
+
+const int MOD = 1e9+7;
+
+static const auto __optimize__ = []() {
+
+std::ios::sync_with_stdio(false);
+
+std::cin.tie(nullptr);
+
+std::cout.tie(nullptr);
+
+return 0;
+
+}();
+
+vector<int> fread_ints(ifstream &in) {
+    vector<int> ans;
+    int data;
+    while (in>>data) {
+        ans.push_back(data);
+    }
+    return ans;
+}
+
+vector<string> fread_strings(ifstream &in) {
+    vector<string> ans;
+    string data;
+    getline(in,data);
+    stringstream ss(data);
+    while(ss>>data){
+        ans.push_back(data);
     }
     return ans;
 }
 
 template<typename T>
-void write_vector(ofstream &out , vector<T> &data) {
+void fwrite_vector(ofstream &out ,vector<T> &data) {
     for(auto i:data) {
         out<<i<<" ";
     }
@@ -34,9 +75,23 @@ int main()
         return 0;
     }
 
+    int m=0, t2=0 ,t3=0, t4=0;
+    in_file>>m>>t2>>t3>>t4;
+    
+    vector<vector<string>> pizza;
+    
     string soln_file_name = "soln_"+testSet;
 
     ofstream out_file(file_path+soln_file_name);
+
+    FOR(i,0,m,1) {
+        int sz=0;
+        in_file>>sz>>ws;
+        auto ing = fread_strings(in_file);
+        cout<<sz<<" "<<ing.size()<<" ";
+        pizza.push_back(ing);
+        fwrite_vector(out_file,ing);
+    }
 
     cout<<"\n\t\tsolution saved succesfully\n";
 
